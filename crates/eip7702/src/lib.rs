1404c4fa629f6c98947d20f4cd7596a9151b2e72
Yash Atreya <44857776+yash-atreya@users.noreply.github.com>
2024-08-22 16:24:20 +0530

    nit

--- crates/eip7702/src/lib.rs
+++ crates/eip7702/src/lib.rs
@@ -1,8 +1,10 @@
 //! [EIP-7702] constants, helpers, and types.
 //!
 //! [EIP-7702]: https://eips.ethereum.org/EIPS/eip-7702
+#![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(not(feature = "std"))]
+#[allow(unused_imports)]
+#[macro_use]
 extern crate alloc;
 
 mod auth_list;
