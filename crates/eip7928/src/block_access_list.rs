3d00555158ddca0b8ce2823565007b339792970d
figtracer <1gusredo@gmail.com>
2025-11-20 19:29:13 +0000

    feat(eip7928): make BAL a dedicated type

--- crates/eip7928/src/block_access_list.rs
+++ crates/eip7928/src/block_access_list.rs
@@ -1,15 +1,85 @@
 //! Contains the [`BlockAccessList`] type, which represents a simple list of account changes.
 
 use crate::account_changes::AccountChanges;
-use alloc::vec::Vec;
+use alloc::vec::{IntoIter, Vec};
+use core::{ops::Deref, slice::Iter};
 
-/// This struct is used to store `account_changes` in a block.
-pub type BlockAccessList = Vec<AccountChanges>;
+/// Represents the full set of [`AccountChanges`] for a block.
+#[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "borsh", derive(borsh::BorshSerialize, borsh::BorshDeserialize))]
+
+pub struct BlockAccessList(Vec<AccountChanges>);
+
+impl From<BlockAccessList> for Vec<AccountChanges> {
+    fn from(this: BlockAccessList) -> Self {
+        this.0
+    }
+}
+
+impl From<Vec<AccountChanges>> for BlockAccessList {
+    fn from(list: Vec<AccountChanges>) -> Self {
+        Self(list)
+    }
+}
+
+impl Deref for BlockAccessList {
+    type Target = [AccountChanges];
+
+    fn deref(&self) -> &Self::Target {
+        self.as_slice()
+    }
+}
+
+impl IntoIterator for BlockAccessList {
+    type Item = AccountChanges;
+    type IntoIter = IntoIter<AccountChanges>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+impl BlockAccessList {
+    /// Creates a new [`BlockAccessList`] from the provided account changes.
+    pub fn new(account_changes: Vec<AccountChanges>) -> Self {
+        Self(account_changes)
+    }
+
+    /// Adds a new [`AccountChanges`] entry to the list.
+    pub fn push(&mut self, account_changes: AccountChanges) {
+        self.0.push(account_changes)
+    }
+
+    /// Returns `true` if the list contains no elements.
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// Returns the number of account change entries contained in the list.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    /// Returns an iterator over the [`AccountChanges`] entries.
+    #[inline]
+    pub fn iter(&self) -> Iter<'_, AccountChanges> {
+        self.0.iter()
+    }
+
+    /// Returns a slice of the contained [`AccountChanges`].
+    #[inline]
+    pub fn as_slice(&self) -> &[AccountChanges] {
+        self.0.as_slice()
+    }
+}
 
 /// Computes the hash of the given block access list.
 #[cfg(feature = "rlp")]
-pub fn compute_block_access_list_hash(bal: &[AccountChanges]) -> alloy_primitives::B256 {
+pub fn compute_block_access_list_hash(bal: &BlockAccessList) -> alloy_primitives::B256 {
     let mut buf = Vec::new();
-    alloy_rlp::encode_list(bal, &mut buf);
+    alloy_rlp::encode_list(bal.as_slice(), &mut buf);
     alloy_primitives::keccak256(&buf)
 }
